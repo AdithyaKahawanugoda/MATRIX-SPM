8c948232d1dd9996a6fb2bbff687fb838a21bdac
randi480 <68816532+randi480@users.noreply.github.com>
2021-08-19 20:12:30 +0530

    Aded Newsletter backend

--- backend/controllers/admin-controller.js
+++ backend/controllers/admin-controller.js
@@ -1,1 +1,94 @@
 const AdminModel = require("../models/admin-model");
+const NewsletterModal = require("../models/newsletter-model");
+const { cloudinary } = require("../utils/cloudinary");
+
+exports.createNewsletter = async (req, res) => {
+  const { title, description, additionalData, fileEnc, tag } = req.body;
+
+  try {
+    const ppUploadRes = await cloudinary.uploader.upload(fileEnc, {
+      upload_preset: "Newsletter_Covers",
+    });
+    const Newsletter = await NewsletterModal.create({
+      title,
+      description,
+      additionalData,
+      tag,
+      coverImage: {
+        imagePublicId: ppUploadRes.public_id,
+        imageSecURL: ppUploadRes.secure_url,
+      },
+    });
+
+    res.status(201).json({ success: true, Newsletter: Newsletter });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      desc: "Error in creating newsletter-" + error,
+    });
+  }
+};
+
+exports.getNewsletters = async (req, res) => {
+  try {
+    const Newsletters = await NewsletterModal.find();
+    res.status(200).send({ Newsletters: Newsletters });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      desc: "Error in fetching Newsletters -" + err,
+    });
+  }
+};
+
+exports.updateNewsletter = async (req, res) => {
+  const { NID, title, description, additionalData, tag } = req.body;
+
+  try {
+    const Newsletter = await NewsletterModal.findByIdAndUpdate(
+      NID,
+      {
+        $set: {
+          title,
+          description,
+          additionalData,
+          additionalData,
+          tag,
+        },
+      },
+      {
+        new: true,
+        upsert: false,
+        omitUndefined: true,
+      }
+    );
+    res.status(200).send({
+      success: true,
+      desc: "newsletter updated successfully",
+      Newsletter,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      desc: "Error in update newsletter" + error,
+    });
+  }
+};
+
+exports.deleteNewsletter = async (req, res) => {
+  const { NID } = req.body;
+
+  try {
+    await NewsletterModal.deleteOne({ _id: NID });
+
+    res.status(200).send({
+      success: true,
+      desc: "newsletter deleted",
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      desc: "Error delete newsletter - " + err,
+    });
+  }
+};
--- backend/routes/admin-routes.js
+++ backend/routes/admin-routes.js
@@ -2,5 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 // route code
+const {
+  createNewsletter,
+  updateNewsletter,
+  getNewsletters,
+  deleteNewsletter
+} = require("../controllers/admin-controller");
+
+router.route("/createNewsletter").post(createNewsletter);
+router.route("/updateNewsletter").put(updateNewsletter);
+router.route("/getNewsletters").get(getNewsletters);
+router.route("/deleteNewsletter").delete(deleteNewsletter);
 
 module.exports = router;
