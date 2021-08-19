97cfdaf01cc1fdf086eafbfcb61484e818d96780
AdithyaKahawanugoda <adithyakahawanugoda@gmail.com>
2021-08-19 16:41:52 +0530

    registration-screen-configured

--- frontend/src/screens/Registration.js
+++ frontend/src/screens/Registration.js
@@ -3,18 +3,29 @@ import Grid from "@material-ui/core/Grid";
 import RegistrationForm from "../components/Adithya/RegistrationForm";
 const Registration = () => {
   return (
-    <>
-      <Grid container spacing={3} className=" lg:px-20 md:px-12 sm:px-4">
-        <Grid item xs={12} sm={12} md={9}>
-          <div className="border border-black border-2 m-1">
+    <div className="lg:px-20 md:px-9 sm:px-4">
+      <Grid
+        container
+        spacing={3}
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item xs={12} sm={12} md={7}>
+          <div className="border border-lightSilver rounded-2xl border-8 shadow-2xl my-8 lg:pl-8 pt-6">
             <RegistrationForm />
           </div>
         </Grid>
-        <Grid item xs={12} sm={12} md={3}>
-          <div className="border border-black border-2 m-1">Static Img</div>
+        <Grid item xs={12} sm={12} md={4}>
+          <div className="my-8 text-cente">
+            <img
+              src="https://i.ibb.co/6tM2Yyt/rd9026-large-0-005ba5ba5ba5ba-733-733.jpg"
+              alt="reg-page-img"
+            />
+          </div>
         </Grid>
       </Grid>
-    </>
+    </div>
   );
 };
 
