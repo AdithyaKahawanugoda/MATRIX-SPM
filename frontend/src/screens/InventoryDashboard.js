72b2ec5c2cacb9930b3ed494d6ade7901dbf59d9
AdithyaKahawanugoda <adithyakahawanugoda@gmail.com>
2021-08-24 08:34:41 +0530

    responsive drawer fixed

--- frontend/src/screens/InventoryDashboard.js
+++ frontend/src/screens/InventoryDashboard.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
 import AppBar from "@material-ui/core/AppBar";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -9,6 +9,10 @@ import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
+
+import MenuIcon from "@material-ui/icons/Menu";
+import Hidden from "@material-ui/core/Hidden";
+import IconButton from "@material-ui/core/IconButton";
 
 import InventoryAddNew from "../components/Adithya/InventoryAddNew";
 import InventoryEdit from "../components/Adithya/InventoryEdit";
@@ -23,21 +27,27 @@ const InventoryDashboardStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
   },
+  drawer: {
+    [theme.breakpoints.up("md")]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
     backgroundColor: "#042B58",
+    [theme.breakpoints.up("md")]: {},
   },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("md")]: {
+      display: "none",
+    },
   },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
   drawerPaper: {
     width: drawerWidth,
-  },
-  drawerContainer: {
-    overflow: "auto",
-    backgroundColor: "#D3DCDE",
-    height: "1000px",
   },
   content: {
     flexGrow: 1,
@@ -45,8 +55,12 @@ const InventoryDashboardStyles = makeStyles((theme) => ({
   },
 }));
 
-const InventoryDashboard = () => {
+const InventoryDashboard = (props) => {
+  const { window } = props;
   const classes = InventoryDashboardStyles();
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = useState(false);
+
   const [inventoryOpen, setInventoryOpen] = useState(true);
   const [inventoryAddNewOpen, setInventoryAddNewOpen] = useState(false);
   const [inventoryEditOpen, setInventoryEditOpen] = useState(false);
@@ -54,11 +68,137 @@ const InventoryDashboard = () => {
   const [wholesalesEditOpen, setWholesalesEditOpen] = useState(false);
   const [profileOpen, setProfileOpen] = useState(false);
 
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <div class="bg-lightSilver " style={{ height: "1000px" }}>
+      <div className={classes.toolbar} />
+      <div class="mt-10 m-auto mb-3 w-3/4 flex justify-center">
+        <img
+          src="https://i.ibb.co/1qjCgjz/download.jpg"
+          alt="profile-pic-inventory-manager"
+          width="100%"
+          height="100%"
+          border="0"
+          className="object-contain rounded-full"
+        ></img>
+      </div>
+      <div class="mt-2 mx-5 mb-8">
+        <Typography
+          variant="h6 text"
+          noWrap
+          class="font-extrabold text-xl ml-2"
+        >
+          Mr. Adithya Kahawanugoda
+        </Typography>
+      </div>
+      <List>
+        <ListItem
+          button
+          onClick={() => {
+            setInventoryAddNewOpen(false);
+            setInventoryEditOpen(false);
+            setWholesalesOpen(false);
+            setWholesalesEditOpen(false);
+            setProfileOpen(false);
+            setInventoryOpen(true);
+          }}
+        >
+          <ListItemText primary={"Inventory"} />
+        </ListItem>
+        <Divider />
+        <ListItem
+          button
+          onClick={() => {
+            setInventoryEditOpen(false);
+            setWholesalesOpen(false);
+            setWholesalesEditOpen(false);
+            setProfileOpen(false);
+            setInventoryOpen(false);
+            setInventoryAddNewOpen(true);
+          }}
+        >
+          <ListItemText primary={"Add New"} />
+        </ListItem>
+        <Divider />
+        <ListItem
+          button
+          onClick={() => {
+            setInventoryAddNewOpen(false);
+            setWholesalesOpen(false);
+            setWholesalesEditOpen(false);
+            setProfileOpen(false);
+            setInventoryOpen(false);
+            setInventoryEditOpen(true);
+          }}
+        >
+          <ListItemText primary={"Inventory Edit / Delete"} />
+        </ListItem>
+        <Divider />
+        <ListItem
+          button
+          onClick={() => {
+            setInventoryAddNewOpen(false);
+            setInventoryEditOpen(false);
+            setWholesalesEditOpen(false);
+            setProfileOpen(false);
+            setInventoryOpen(false);
+            setWholesalesOpen(true);
+          }}
+        >
+          <ListItemText primary={"Wholesales"} />
+        </ListItem>
+        <Divider />
+        <ListItem
+          button
+          onClick={() => {
+            setInventoryAddNewOpen(false);
+            setInventoryEditOpen(false);
+            setWholesalesOpen(false);
+            setProfileOpen(false);
+            setInventoryOpen(false);
+            setWholesalesEditOpen(true);
+          }}
+        >
+          <ListItemText primary={"Wholesales Edit / Delete"} />
+        </ListItem>
+        <Divider />
+        <ListItem
+          button
+          onClick={() => {
+            setInventoryAddNewOpen(false);
+            setInventoryEditOpen(false);
+            setWholesalesOpen(false);
+            setWholesalesEditOpen(false);
+            setInventoryOpen(false);
+            setProfileOpen(true);
+          }}
+        >
+          <ListItemText primary={"Profile"} />
+        </ListItem>
+      </List>
+    </div>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
   return (
     <div className={classes.root}>
       <CssBaseline />
       <AppBar position="fixed" className={classes.appBar}>
         <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
           <Typography
             variant="h6 text"
             noWrap
@@ -68,125 +208,39 @@ const InventoryDashboard = () => {
           </Typography>
         </Toolbar>
       </AppBar>
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <Toolbar />
-        <div className={classes.drawerContainer}>
-          <div class="mt-10 m-auto mb-3 w-3/4 flex justify-center">
-            <img
-              src="https://i.ibb.co/1qjCgjz/download.jpg"
-              alt="profile-pic-inventory-manager"
-              width="100%"
-              height="100%"
-              border="0"
-              className="object-contain rounded-full"
-            ></img>
-          </div>
-          <div class="mt-2 mx-5 mb-8">
-            <Typography
-              variant="h6 text"
-              noWrap
-              class="font-extrabold text-xl ml-2"
-            >
-              Mr. Adithya Kahawanugoda
-            </Typography>
-          </div>
-          <div style={{ marginTop: "50px" }}>
-            <List>
-              <ListItem
-                button
-                onClick={() => {
-                  setInventoryAddNewOpen(false);
-                  setInventoryEditOpen(false);
-                  setWholesalesOpen(false);
-                  setWholesalesEditOpen(false);
-                  setProfileOpen(false);
-                  setInventoryOpen(true);
-                }}
-              >
-                <ListItemText primary={"Inventory"} />
-              </ListItem>
-              <Divider />
-              <ListItem
-                button
-                onClick={() => {
-                  setInventoryEditOpen(false);
-                  setWholesalesOpen(false);
-                  setWholesalesEditOpen(false);
-                  setProfileOpen(false);
-                  setInventoryOpen(false);
-                  setInventoryAddNewOpen(true);
-                }}
-              >
-                <ListItemText primary={"Add New"} />
-              </ListItem>
-              <Divider />
-              <ListItem
-                button
-                onClick={() => {
-                  setInventoryAddNewOpen(false);
-                  setWholesalesOpen(false);
-                  setWholesalesEditOpen(false);
-                  setProfileOpen(false);
-                  setInventoryOpen(false);
-                  setInventoryEditOpen(true);
-                }}
-              >
-                <ListItemText primary={"Inventory Edit / Delete"} />
-              </ListItem>
-              <Divider />
-              <ListItem
-                button
-                onClick={() => {
-                  setInventoryAddNewOpen(false);
-                  setInventoryEditOpen(false);
-                  setWholesalesEditOpen(false);
-                  setProfileOpen(false);
-                  setInventoryOpen(false);
-                  setWholesalesOpen(true);
-                }}
-              >
-                <ListItemText primary={"Wholesales"} />
-              </ListItem>
-              <Divider />
-              <ListItem
-                button
-                onClick={() => {
-                  setInventoryAddNewOpen(false);
-                  setInventoryEditOpen(false);
-                  setWholesalesOpen(false);
-                  setProfileOpen(false);
-                  setInventoryOpen(false);
-                  setWholesalesEditOpen(true);
-                }}
-              >
-                <ListItemText primary={"Wholesales Edit / Delete"} />
-              </ListItem>
-              <Divider />
-              <ListItem
-                button
-                onClick={() => {
-                  setInventoryAddNewOpen(false);
-                  setInventoryEditOpen(false);
-                  setWholesalesOpen(false);
-                  setWholesalesEditOpen(false);
-                  setInventoryOpen(false);
-                  setProfileOpen(true);
-                }}
-              >
-                <ListItemText primary={"Profile"} />
-              </ListItem>
-            </List>
-          </div>
-        </div>
-      </Drawer>
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Hidden smUp implementation="css">
+          <Drawer
+            container={container}
+            variant="temporary"
+            anchor={theme.direction === "rtl" ? "right" : "left"}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden smDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
       <main className={classes.content}>
-        <Toolbar />
+        <div className={classes.toolbar} />
         {inventoryOpen && <InventoryInStock />}
         {inventoryAddNewOpen && <InventoryAddNew />}
         {inventoryEditOpen && <InventoryEdit />}
