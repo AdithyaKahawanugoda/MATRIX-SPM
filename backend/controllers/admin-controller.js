983db7f17565b4c3096143ad8ccc18fa1f39bb00
Deshani <it19246024@my.sliit.lk>
2021-09-03 23:25:40 +0530

    request book backend commit

--- backend/controllers/admin-controller.js
+++ backend/controllers/admin-controller.js
@@ -1,1 +1,14 @@
 const AdminModel = require("../models/admin-model");
+const RequestBook = require("../models/requestBook-model");
+
+exports.getBookRequests = async (req, res) => {
+    try {
+      const bookRequests = await RequestBook.find();
+      res.status(200).send({ bookRequests: bookRequests });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        desc: "Error in fetching book requests -" + err,
+      });
+    }
+  };
--- backend/routes/admin-routes.js
+++ backend/routes/admin-routes.js
@@ -2,5 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 // route code
+const {
+    getBookRequests,
+} = require("../controllers/admin-controller");
+
+
+router.route("/getBookRequests").get(getBookRequests);
 
 module.exports = router;
