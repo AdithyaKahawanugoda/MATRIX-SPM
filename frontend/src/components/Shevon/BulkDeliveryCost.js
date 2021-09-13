8ae455130d8972a95c17685664fd68d1cc92782c
shevon <shevonkrishmal1998@gmail.com>
2021-09-14 01:52:05 +0530

    Retrieve bulk cost

--- frontend/src/components/Shevon/BulkDeliveryCost.js
+++ frontend/src/components/Shevon/BulkDeliveryCost.js
@@ -1,17 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import EditIcon from "@material-ui/icons/Edit";
 import BulkCostEditModal from "./modals/BulkCostEditModal";
 import BulkExCostModal from "./modals/BulkExCostModal";
+import Icon from "@material-ui/core/Icon";
 
 const BulkDeliveryCost = () => {
   const [bulkCostEditOpen, setbulkCostEditOpen] = useState(false);
   const [bulkExCostOpen, setBulkExCostEditOpen] = useState(false);
+
+  const [bulkCost, setBulkCost] = useState([]);
+  const [selectedBulkCost, setSelectedBulkCost] = useState();
+
+  const getBulk = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:6500/matrix/api/deliveryManager/getbulk"
+      );
+      setBulkCost(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getBulk();
+  }, []);
+
   return (
     <div>
       <div className="grid grid-cols-2 ">
         <div
           className="rounded-xl   mt-2 mx-0 px-0 py-0  border-0  shadow-md bg-lightSilver bg-opacity-20 overflow-y-auto "
-          style={{ maxHeight: "490px" }}
+          style={{ height: "490px" }}
         >
           <div className="rounded-xl   my-2 mx-2 px-5 py-5  border-0  shadow-md bg-prussianBlue bg-opacity-20  ">
             <div className="grid grid-cols-2">
@@ -21,7 +42,6 @@ const BulkDeliveryCost = () => {
               <div className="text-black ">
                 <div className="grid grid-cols-2">
                   <div className="ml-32">
-                    {" "}
                     <h5 className="mr-14"> 10% </h5>{" "}
                   </div>
                   <div
@@ -31,7 +51,6 @@ const BulkDeliveryCost = () => {
                       setBulkExCostEditOpen(true);
                     }}
                   >
-                    {" "}
                     <EditIcon />
                   </div>
                 </div>
@@ -40,238 +59,37 @@ const BulkDeliveryCost = () => {
           </div>
 
           <h5 className="ml-4 mt-8 font-black">Cost per weight(kg)</h5>
-          <div className="rounded-xl   my-2 mx-2 px-5 py-5  border-0  shadow-md bg-blueSapphire ">
-            <div className="grid grid-cols-2">
-              <div>
-                <h5 className="text-white">Western Province :</h5>
-              </div>
-              <div className="text-white ">
-                <div className="grid grid-cols-2">
-                  <div className="ml-32">
-                    {" "}
-                    <h5 className="mr-14">Rs.100.00 </h5>{" "}
-                  </div>
-                  <div
-                    className="text-right"
-                    onClick={() => {
-                      setBulkExCostEditOpen(false);
-                      setbulkCostEditOpen(true);
-                    }}
-                  >
-                    {" "}
-                    <EditIcon />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
 
-          <div className="rounded-xl   my-2 mx-2 px-5 py-5  border-0  shadow-md bg-blueSapphire ">
-            <div className="grid grid-cols-2">
-              <div>
-                <h5 className="text-white">Central Province :</h5>
-              </div>
-              <div className="text-white ">
-                <div className="grid grid-cols-2">
-                  <div className="ml-32">
-                    {" "}
-                    <h5 className="mr-14">Rs.500.00 </h5>{" "}
-                  </div>
-                  <div
-                    className="text-right"
-                    onClick={() => {
-                      setBulkExCostEditOpen(false);
-                      setbulkCostEditOpen(true);
-                    }}
-                  >
-                    {" "}
-                    <EditIcon />
+          {bulkCost.map((bulkCost, index) => (
+            <div
+              className="rounded-xl   my-2 mx-2 px-5 py-5  border-0  shadow-md bg-blueSapphire "
+              key={index}
+            >
+              <div className="grid grid-cols-2">
+                <div>
+                  <h5 className="text-white mt-2">{bulkCost.provincename} :</h5>
+                </div>
+                <div className="text-white ">
+                  <div className="grid grid-cols-2">
+                    <div className="ml-32">
+                      <h5 className="mr-14 mt-2">Rs.{bulkCost.cost}.00</h5>
+                    </div>
+                    <div className="text-right">
+                      <Icon
+                        onClick={() => {
+                          setBulkExCostEditOpen(false);
+                          setbulkCostEditOpen(true);
+                          setSelectedBulkCost(bulkCost);
+                        }}
+                      >
+                        <EditIcon />
+                      </Icon>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-
-          <div className="rounded-xl   my-2 mx-2 px-5 py-5  border-0  shadow-md bg-blueSapphire ">
-            <div className="grid grid-cols-2">
-              <div>
-                <h5 className="text-white">Eastern Province :</h5>
-              </div>
-              <div className="text-white ">
-                <div className="grid grid-cols-2">
-                  <div className="ml-32">
-                    {" "}
-                    <h5 className="mr-14">Rs.300.00 </h5>{" "}
-                  </div>
-                  <div
-                    className="text-right"
-                    onClick={() => {
-                      setBulkExCostEditOpen(false);
-                      setbulkCostEditOpen(true);
-                    }}
-                  >
-                    {" "}
-                    <EditIcon />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="rounded-xl   my-2 mx-2 px-5 py-5  border-0  shadow-md bg-blueSapphire ">
-            <div className="grid grid-cols-2">
-              <div>
-                <h5 className="text-white">Northern Province :</h5>
-              </div>
-              <div className="text-white ">
-                <div className="grid grid-cols-2">
-                  <div className="ml-32">
-                    {" "}
-                    <h5 className="mr-14">Rs.200.00 </h5>{" "}
-                  </div>
-                  <div
-                    className="text-right"
-                    onClick={() => {
-                      setBulkExCostEditOpen(false);
-                      setbulkCostEditOpen(true);
-                    }}
-                  >
-                    {" "}
-                    <EditIcon />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="rounded-xl   my-2 mx-2 px-5 py-5  border-0  shadow-md bg-blueSapphire ">
-            <div className="grid grid-cols-2">
-              <div>
-                <h5 className="text-white">Southern Province :</h5>
-              </div>
-              <div className="text-white ">
-                <div className="grid grid-cols-2">
-                  <div className="ml-32">
-                    {" "}
-                    <h5 className="mr-14">Rs.200.00 </h5>{" "}
-                  </div>
-                  <div
-                    className="text-right"
-                    onClick={() => {
-                      setBulkExCostEditOpen(false);
-                      setbulkCostEditOpen(true);
-                    }}
-                  >
-                    {" "}
-                    <EditIcon />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="rounded-xl   my-2 mx-2 px-5 py-5  border-0  shadow-md bg-blueSapphire ">
-            <div className="grid grid-cols-2">
-              <div>
-                <h5 className="text-white">North Western Province :</h5>
-              </div>
-              <div className="text-white ">
-                <div className="grid grid-cols-2">
-                  <div className="ml-32">
-                    {" "}
-                    <h5 className="mr-14">Rs.200.00 </h5>{" "}
-                  </div>
-                  <div
-                    className="text-right"
-                    onClick={() => {
-                      setBulkExCostEditOpen(false);
-                      setbulkCostEditOpen(true);
-                    }}
-                  >
-                    {" "}
-                    <EditIcon />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="rounded-xl   my-2 mx-2 px-5 py-5  border-0  shadow-md bg-blueSapphire ">
-            <div className="grid grid-cols-2">
-              <div>
-                <h5 className="text-white">North Central Province :</h5>
-              </div>
-              <div className="text-white ">
-                <div className="grid grid-cols-2">
-                  <div className="ml-32">
-                    {" "}
-                    <h5 className="mr-14">Rs.400.00 </h5>{" "}
-                  </div>
-                  <div
-                    className="text-right"
-                    onClick={() => {
-                      setBulkExCostEditOpen(false);
-                      setbulkCostEditOpen(true);
-                    }}
-                  >
-                    {" "}
-                    <EditIcon />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="rounded-xl   my-2 mx-2 px-5 py-5  border-0  shadow-md bg-blueSapphire ">
-            <div className="grid grid-cols-2">
-              <div>
-                <h5 className="text-white">Uva Province :</h5>
-              </div>
-              <div className="text-white ">
-                <div className="grid grid-cols-2">
-                  <div className="ml-32">
-                    {" "}
-                    <h5 className="mr-14">Rs.700.00 </h5>{" "}
-                  </div>
-                  <div
-                    className="text-right"
-                    onClick={() => {
-                      setBulkExCostEditOpen(false);
-                      setbulkCostEditOpen(true);
-                    }}
-                  >
-                    <EditIcon />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="rounded-xl   my-2 mx-2 px-5 py-5  border-0  shadow-md bg-blueSapphire ">
-            <div className="grid grid-cols-2">
-              <div>
-                <h5 className="text-white">Sabaragamuwa Province :</h5>
-              </div>
-              <div className="text-white ">
-                <div className="grid grid-cols-2">
-                  <div className="ml-32">
-                    {" "}
-                    <h5 className="mr-14">Rs.800.00 </h5>{" "}
-                  </div>
-                  <div
-                    className="text-right"
-                    onClick={() => {
-                      setBulkExCostEditOpen(false);
-                      setbulkCostEditOpen(true);
-                    }}
-                  >
-                    {" "}
-                    <EditIcon />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
 
         <div
@@ -286,6 +104,8 @@ const BulkDeliveryCost = () => {
         <BulkCostEditModal
           modalVisible={bulkCostEditOpen}
           setModalVisible={setbulkCostEditOpen}
+          selectedBulkCost={{ ...selectedBulkCost }}
+          setBulkCost={setBulkCost}
         />
       )}
 
