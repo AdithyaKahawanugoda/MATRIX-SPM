bd9dc01a2d32331a0c986bf952d071b8e5c3c2cc
AdithyaKahawanugoda <adithyakahawanugoda@gmail.com>
2021-09-20 10:55:03 +0530

    admin login implemented

--- frontend/src/screens/AdminLogin.js
+++ frontend/src/screens/AdminLogin.js
@@ -1,7 +1,16 @@
 import React from "react";
+import AdminLoginForm from "../components/Adithya/AdminLoginForm";
+import Footer from "../components/Adithya/Footer";
+import Header from "../components/Adithya/Header";
 
 const AdminLogin = () => {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <AdminLoginForm />
+      <Footer />
+    </>
+  );
 };
 
 export default AdminLogin;
